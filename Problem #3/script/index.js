d74fcebb4f3968e6aff336cb32aeed6f85f8fcbd
playerB <skbrave1@gmail.com>
2021-06-18 15:45:17 +0700

    Add password confirmation

--- Problem #3/script/index.js
+++ Problem #3/script/index.js
@@ -1,15 +1,25 @@
-const form = document.getElementById("register-form")
-form.addEventListener("submit", event => {
-  event.preventDefault()
-  const formData = new FormData(form)
-  const data = {}
-  for (const [key, value] of formData.entries()) {
-    /* USER CODE Begin: Validate data */
-    data[key] = value
-    /* USER CODE Begin: Validate data */
-  }
-  console.log(data)
-  /* USER CODE Begin: What happened next after recieve form data (Optional) */
+const form = document.getElementById("register-form");
+form.addEventListener("submit", (event) => {
+    //event.preventDefault();
+    const formData = new FormData(form);
+    const data = {};
+    for (const [key, value] of formData.entries()) {
+        /* USER CODE Begin: Validate data */
+        data[key] = value;
+        if (data["password"] != data["confirmpassword"]) {
+			document
+                .getElementById("confirmpassword")
+                .setCustomValidity("Password confirmation mismatch");
+            document.getElementById("confirmpassword").reportValidity();
+        } else {
+            document.getElementById("confirmpassword").setCustomValidity('');
+			document.getElementById("confirmpassword").reportValidity();
+        }
+        /* USER CODE Begin: Validate data */
+    }
+    console.log(data);
+	console.log(data["password"] != data["confirmpassword"]);
+    /* USER CODE Begin: What happened next after recieve form data (Optional) */
 
-  /* USER CODE END: What happened next after recieve form data (Optional) */
-})
+    /* USER CODE END: What happened next after recieve form data (Optional) */
+});
